0eaeb9ed92241de26103be220511d10cddc4a0a0
SegFault42 <SegFault42@protonmail.com>
2019-06-14 07:42:08 +0200

    add print_info

--- libusb/main.c
+++ libusb/main.c
@@ -3,23 +3,48 @@
 #include <libusb.h>
 #include <stdio.h>
 
+int	print_info(libusb_device *dev)
+{
+	struct libusb_device_descriptor	desc;
+	int							r = 0;
+
+	r = libusb_get_device_descriptor(dev, &desc);
+	if (r < 0) {
+		printf("libusb_get_device_descriptor() failed\n");
+		return (-1);
+	}
+
+	printf("%x\n", desc.idVendor);
+
+	return (0);
+}
+
 int	main(void)
 {
 	int				init = 0;
-	ssize_t			err = 0;
+	ssize_t			dev = 0;
 	libusb_context	*ctx = NULL;
 	libusb_device	**list = NULL;
 
 	init = libusb_init(&ctx);
 
-	if (init != 0)
+	if (init < 0) {
+		printf("Init failed\n");
 		return (-1);
-
-	if ((err = libusb_get_device_list(ctx, &list)) < 0) {
-		libusb_strerror(err);
 	}
 
-	printf("%d\n", err);
+	libusb_set_debug(ctx, 3);
 
+	dev = libusb_get_device_list(ctx, &list);
+	if (dev < 0) {
+		libusb_strerror(dev);
+	}
+
+	printf("%d\n", dev);
+
+	print_info(list[0]);
+
+	libusb_free_device_list(list, 1);
+	libusb_exit(ctx);
 	return (0);
 }
