fc92b765cceb962826ea531731b24e204989f157
SegFault42 <SegFault42@protonmail.com>
2019-07-05 10:40:38 +0200

    update dropbox upload

--- dropbox_auth/main.c
+++ dropbox_auth/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
+
+#define REQUEST_LENGHT 101
 
 int main(void)
 {
@@ -7,6 +11,15 @@ int main(void)
 	CURLcode			res;
 	struct curl_slist	*chunk = NULL;
 	char				*data = "Hello\n";
+	char				*filename = "/file.txt";
+	char				*filename_request = NULL;
+
+	filename_request = (char *)calloc(REQUEST_LENGHT + strlen(filename) + 1, sizeof(char));
+	if (filename_request == NULL) {
+		return (-1);
+	}
+
+	sprintf(filename_request, "Dropbox-API-Arg: {\"path\": \"%s\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}", filename);
 
   	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -19,8 +32,8 @@ int main(void)
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
 		// append all header
-		chunk = curl_slist_append(chunk, "Authorization: Bearer <token here>");
-		chunk = curl_slist_append(chunk, "Dropbox-API-Arg: {\"path\": \"/filename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}");
+		chunk = curl_slist_append(chunk, "Authorization: Bearer <TOKEN_HERE>");
+		chunk = curl_slist_append(chunk, filename_request);
 		chunk = curl_slist_append(chunk, "Content-Type: application/octet-stream");
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
 
@@ -37,5 +50,7 @@ int main(void)
     	curl_easy_cleanup(curl);
   	}
   	curl_global_cleanup();
+
+	free(filename_request);
   	return 0;
 }
