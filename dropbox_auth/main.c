f961d4ed621d368cebf425f4a9be22cd86f03fa1
SegFault42 <SegFault42@protonmail.com>
2019-07-10 22:50:40 +0200

    silent output

--- dropbox_auth/main.c
+++ dropbox_auth/main.c
@@ -5,6 +5,11 @@
 
 #define REQUEST_LENGHT 101
 
+size_t func(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	return size * nmemb;
+}
+
 int main(void)
 {
 	CURL				*curl;
@@ -21,20 +26,24 @@ int main(void)
 
 	sprintf(filename_request, "Dropbox-API-Arg: {\"path\": \"%s\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}", filename);
 
-  	curl_global_init(CURL_GLOBAL_ALL);
+	curl_global_init(CURL_GLOBAL_ALL);
 
-  	curl = curl_easy_init();
+	curl = curl_easy_init();
 
-  	if(curl) {
+	if(curl) {
 
 		// data to send (set size and content)
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, 6L);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
+		// silent output
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &func);
+
 		// append all header
 		chunk = curl_slist_append(chunk, "Authorization: Bearer _G3bZFuCKiAAAAAAAAAADbubss3eXFcWBIJb5awT7_zNVKUuh68WHN_G8BHsPbpc");
 		chunk = curl_slist_append(chunk, filename_request);
 		chunk = curl_slist_append(chunk, "Content-Type: application/octet-stream");
+
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
 
     	curl_easy_setopt(curl, CURLOPT_URL, "https://content.dropboxapi.com/2/files/upload");
@@ -50,7 +59,7 @@ int main(void)
     	curl_easy_cleanup(curl);
   	}
   	curl_global_cleanup();
-	 curl_slist_free_all(chunk);
+	curl_slist_free_all(chunk);
 
 	free(filename_request);
   	return 0;
