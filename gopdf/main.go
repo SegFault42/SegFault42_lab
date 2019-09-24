6568c41a5e9e7499a0f5d03a2cfe3c041ee31a69
SegFault42 <SegFault42@protonmail.com>
2019-09-24 11:40:39 +0200

    add multiple pagfes

--- gopdf/main.go
+++ gopdf/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "github.com/jung-kurt/gofpdf"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
 
 func main() {
-	pdf := gofpdf.New("P", "mm", "A4", "")
+	// Create new pdf document
+	//pdf := gofpdf.New("P", "mm", "A4", "")
+
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if filepath.Ext(f.Name()) == ".jpg" {
+			fmt.Println(f.Name())
+		}
+	}
+
 	pdf.AddPage()
-	pdf.Image("001.jpg", 0, 0, 200, 0, false, "", 0, "")
-	fileStr := "Fpdf_Image.pdf"
-	pdf.OutputFileAndClose(fileStr)
+	//pdf.Image("001.jpg", 5, 0, 200, 0, false, "", 0, "")
+	//pdf.AddPage()
+	//pdf.Image("002.jpg", 5, 0, 200, 0, false, "", 0, "")
+	//fileStr := "Fpdf_Image.pdf"
+	//pdf.OutputFileAndClose(fileStr)
 }
