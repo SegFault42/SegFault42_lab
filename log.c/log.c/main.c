455b959a8e9ef5be3655033da55b3b89fc5c4657
SegFault42 <SegFault42@protonmail.com>
2020-06-02 15:47:28 +0300

    update log

--- log.c/log.c/main.c
+++ log.c/log.c/main.c
@@ -1,6 +1,24 @@
 #include "./log/log.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-	log_error("This :%s", "is an error\n");
+	FILE *fp = NULL;
+
+	fp = fopen("debug.log", "a+");
+	if (fp == NULL) {
+		log_error("%s", strerror(errno));
+		return (-1);
+	}
+
+	log_set_fp(fp);
+	log_info("==================BEGINING OF SESSION !====================");
+
+	log_info("Success !");
+
+	log_info("=====================END OF SESSION !======================\n\n");
+
+	fclose(fp);
 }
